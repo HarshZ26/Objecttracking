26d1d5ae9babfd5d9c71f8c2d5658eb97137ef32
Harsh Sharma <89373218+HarshZ26@users.noreply.github.com>
2023-03-21 21:34:42 +0530

    Update deepsort.py

--- deepsort.py
+++ deepsort.py
@@ -15,12 +15,15 @@ transform = transforms.Compose([transforms.ToTensor()])
 
 # Load YOLOv5 model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-my_tracker = DeepSort(max_age=5,max_cosine_distance=0.5, nms_max_overlap=1.0)
+classes = model.names
+my_tracker = DeepSort(max_age=30,max_cosine_distance=0.6, nms_max_overlap=1.0)
 
 # Open video file
-cap = cv2.VideoCapture(1) # Change the camera index as per system
-fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-out = cv2.VideoWriter('output.mp4',fourcc, 20.0, (640,480))  
+cap = cv2.VideoCapture(1)
+# Define the codec and create VideoWriter object if you want to save the video
+
+# fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+# out = cv2.VideoWriter('output.mp4',fourcc, 20.0, (640,480))  
 # Define colors for different tracks
 color_list = np.random.randint(0, 255, size=(1000, 3), dtype=np.uint8)
 # Loop through frames
@@ -34,30 +37,30 @@ while True:
     # Detect objects with YOLOv5
     results = model(frame)
     # Extract features for each object
-    detections = []
+    deepsort_detections = []
+    features = []
     for obj in results.xyxy[0]:
         x1, y1, x2, y2, conf, cls = obj.cpu().numpy()
-        if cls==0:
-            cropped = frame[int(y1):int(y2), int(x1):int(x2)]
-            resized = cv2.resize(cropped, (224, 224))
-            tensor = torch.from_numpy(resized).permute(2, 0, 1).float().div(255.0).unsqueeze(0).cuda()
-            with torch.no_grad():
-                feature = model.forward(tensor)
-                feature = feature.squeeze().cpu().numpy()
-            detection = Detection(np.array([x1, y1, int(x2-x1), int(y2-y1)]), conf, feature,class_name='person')
-            detections.append(detection)
+        if conf > 0.3 and cls==0:
+            # #for any pretrained model uncomment this if you have a any
+            # cropped = frame[int(y1):int(y2), int(x1):int(x2)]
+            # resized = cv2.resize(cropped, (224, 224))
+            # tensor = torch.from_numpy(resized).permute(2, 0, 1).float().div(255.0).unsqueeze(0).cuda()
+            # with torch.no_grad():
+            #     feature = model.forward(tensor)
+            #     feature = feature.squeeze().cpu().numpy()
+            # feature = feature[:,-1].squeeze()
+            # features.append(feature)
+            # print(feature.shape)
+            detection = Detection(np.array([x1, y1, int(x2-x1), int(y2-y1)]), conf, feature = None,class_name='person')
+            deepsort_detections.append((list(detection.get_ltwh()), detection.confidence, detection.class_name))
 
-    # Associate objects across frames with SORT and DeepSORT
+    # Associate objects across frames with DeepSORT
     ids = []
     total = 0
-    if len(detections) > 0:
-        # Run SORT tracker
+    if len(deepsort_detections) > 0:
         # print("here",len(detections))
         # Run DeepSORT tracker
-        deepsort_detections = []
-        for detection in detections:
-            detection = (list(detection.get_ltwh()), detection.confidence, detection.class_name)
-            deepsort_detections.append(detection)
         deepsort_tracks = my_tracker.update_tracks(raw_detections = deepsort_detections, frame = frame)
         # Draw bounding boxes and track IDs on frame
         for track in deepsort_tracks:
@@ -78,11 +81,11 @@ while True:
     cv2.putText(frame, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
     cv2.putText(frame, f'Count: {int(total)}', (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
     cv2.imshow('frame', frame)
-    out.write(frame)  
+    # out.write(frame)  
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Release resources
 cap.release()
-out.release()
+# out.release()
 cv2.destroyAllWindows()
